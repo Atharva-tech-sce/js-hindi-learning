315da145c2eca03afa75621fb47c7fa153b08ffb
Atharva-tech-sce <codewithatharva98@gmail.com>
2024-07-15 16:40:16 +0000

    Conversion

---
+++ .devcontainer/01_basics/03_conversionoperations.js
@@ -0,0 +1,38 @@
+// let rollno = 33 //this is number
+// let rollno = "33" // this is string
+// let rollno = "33abc" // this is NaN
+// let rollno = null
+// let rollno = undefined
+// let rollno = true
+// let rollno = false
+// let rollno = "Atharva"
+
+// console.log(typeof (rollno) );
+// console.log(typeof rollno); // both console.log(typeof (rollno) ); and console.log(typeof rollno); are same they give gsame output
+
+// let valueInNumber = Number(rollno)
+ 
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber)//In terminal output NaN means "not a number"
+
+/*If you used null in dobule quote like "null" than let rollno = "null" it will give output Nan because dobule quotes
+will convert it into strings, strings is function that can contain multiple data types */ 
+
+// let isLoggedIn = 1
+// let isLoggedIn = 0
+// let isLoggedIn = ""
+// let isLoggedIn = "Atharva"
+
+// let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false; "Atharva" => true
+
+let somenumber = 33
+
+let stringnumber = String(somenumber)
+console.log(stringnumber)
+console.log(typeof stringnumber)
+
