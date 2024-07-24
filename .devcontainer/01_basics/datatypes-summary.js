879b13a755dd24eba7687d53dffc209d9fb13f85
Atharva-tech-sce <codewithatharva98@gmail.com>
2024-07-24 13:28:44 +0530

    Add files via upload

--- .devcontainer/01_basics/datatypes-summary.js
+++ .devcontainer/01_basics/datatypes-summary.js
@@ -6,11 +6,13 @@ const score = 100
 const scoreValue = 100.3
 
 const isLoggedIn = false
-const outsideTemp = null
-let userEmail;
+const outsideTemp = null // if we will do 'typeof' null we'll get 'object'
+let userEmail; // this will lead to userEmail being undefiend or you can just type userEmail = undefiend
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
+const id = Symbol('123') // to make id unique we use 'Symbol'
+const anotherId = Symbol('123') /* Even though the value in id and anotherId 
+seems same they are not cause symbol make every variable unique and that's why they are not
+same, yoyu can check by running console.log given below  */
 
 console.log(id === anotherId);
 
@@ -24,12 +26,13 @@ console.log(id === anotherId);
 
 const heros = ["shaktiman", "naagraj", "doga"];
 let myObj = {
-    name: "hitesh",
-    age: 22,
+    name: "hitesh", //everything in curly braces are object
+    age: 22, // object can store any datatypes even object can store object in itself
 }
 
+
 const myFunction = function(){
-    console.log("Hello world");
+    console.log("Hello world"); // function is defined as function(){}
 }
 
 console.log(typeof anotherId);
---
+++ .devcontainer/01_basics/datatypes-summary2.js
@@ -0,0 +1,33 @@
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+// OR, both are same                
+let useEmail = undefined
+
+const anotherId = Symbol('123')
+const id = Symbol('123')
+
+console.log(id === anotherId);
+console.log(typeof score);
+
+const heros = ["shaktiman", "naagraj", "doga"];
+
+let myObj = {
+    name: "hitesh", //everything in curly braces are object
+    age: 22, // object can store any datatypes even object can store object in itself
+}
+
+console.log(myObj)
+
+const myFunction = function(){
+    console.log("Hello world"); // function is defined as function(){}
+};
+
+myFunction() 
+/* when you call 'myFunction()' it will call function() an that will call Hello world, 
+But wehn you type in "console.log(myfunction())" this will result in console log => myFunction => function
+=> hello world after that in myFunction() it will not be defiend as anything so will give "undefiend" */
+               
