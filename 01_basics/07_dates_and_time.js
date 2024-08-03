f00ee819a71c465628f92225fa024c1eede32178
Atharva-tech-sce <codewithatharva98@gmail.com>
2024-08-03 01:04:16 +0000

    date and time propeties

---
+++ 01_basics/07_dates_and_time.js
@@ -0,0 +1,18 @@
+// Date
+
+let myDate = new Date()
+console.log(myDate);//to print date and time
+console.log(myDate.toString());//To print date and time in more organized way
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON);
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.toTimeString());
+console.log(myDate.toUTCString());
+console.log(myDate.getTimezoneOffset());
+
+
+
+
