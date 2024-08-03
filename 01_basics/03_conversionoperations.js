bd84439d6c79d2c746593746e156edfef8680138
Atharva-tech-sce <codewithatharva98@gmail.com>
2024-08-03 00:19:49 +0000

    changes saved

--- 01_basics/03_conversionoperations.js
+++ 01_basics/03_conversionoperations.js
@@ -1,14 +1,12 @@
-/*
 
-
-let rollno = 33 //this is number
-let rollno = "33" // this is string
-let rollno = "33abc" // this is NaN
-let rollno = null // this will give "0" as output
-let rollno = undefined // this will give undefined as output
-let rollno = true // it is a boolean function and will give 1 or true as otput
-let rollno = false// it is a boolean function and will give 0 or False as otput
-let rollno = "Atharva"
+// let rollno = 33 //this is number
+// let rollno = "33" // this is string
+// let rollno = "33abc" // this is NaN
+// let rollno = null // this will give "0" as output
+// let rollno = undefined // this will give undefined as output
+// let rollno = true // it is a boolean function and will give 1 or true as otput
+// let rollno = false// it is a boolean function and will give 0 or False as otput
+// let rollno = "Atharva"
 
 console.log(typeof (rollno) );
 console.log(typeof rollno); // both console.log(typeof (rollno) ); and console.log(typeof rollno); are same they give gsame output
@@ -22,12 +20,12 @@ console.log(valueInNumber)//In terminal output NaN means "not a number"
 /*If you used null in dobule quote like "null" than let rollno = "null" it will give output Nan because dobule quotes
 will convert it into strings, strings is function that can contain multiple data types */ 
 
-/*
 
-let isLoggedIn = 1
-let isLoggedIn = 0
-let isLoggedIn = ""
-let isLoggedIn = "Atharva"
+
+// let isLoggedIn = 1
+// let isLoggedIn = 0
+// let isLoggedIn = ""
+// let isLoggedIn = "Atharva"
 
 let booleanisLoggedIn = Boolean(isLoggedIn)
 console.log(booleanisLoggedIn);
@@ -41,48 +39,48 @@ let stringnumber = String(somenumber)
 console.log(stringnumber)
 console.log(typeof stringnumber)
 
-*/
+
 
 
 // ******************************** OPERATIONS ***********************************
 
 let Score = 70
 let negScore = -Score
-// console.log(negScore);
+console.log(negScore);
 
-// console.log(2+2);
-// console.log(2-2);
-// console.log(2/2);
-// console.log(2*2);
-// console.log(2**3); 
-// console.log(2%3);
+console.log(2+2);
+console.log(2-2);
+console.log(2/2);
+console.log(2*2);
+console.log(2**3); 
+console.log(2%3);
 
 
 let str1 = "hello"
 let str2 = "Atharva"
 let str3 = str1 + str2
 
-// console.log(str3);
+console.log(str3);
 
 // Javascript evaluate equation from left to right 
 
-// console.log("1" + 2 ); // here on left side 1 is written in string so it will treat the whole equation as string and will not perform arithmetic operations
-// console.log( 1 + "2" ); // here on right side 2 is written in string so it will treat the 1 as number but 2 as string and will not perform arithmetic operations
-// console.log( "1" + 2 + 2 ); // here on left side 1 is written in string so it will treat the whole equation as string and will not perform arithmetic operations
-// console.log( 1 + 2 + "2" ); //here on left side both are numbers and the javascript will add both of them but will treat 2 as string and won't perform arithmetic opertions with it
+console.log("1" + 2 ); // here on left side 1 is written in string so it will treat the whole equation as string and will not perform arithmetic operations
+console.log( 1 + "2" ); // here on right side 2 is written in string so it will treat the 1 as number but 2 as string and will not perform arithmetic operations
+console.log( "1" + 2 + 2 ); // here on left side 1 is written in string so it will treat the whole equation as string and will not perform arithmetic operations
+console.log( 1 + 2 + "2" ); //here on left side both are numbers and the javascript will add both of them but will treat 2 as string and won't perform arithmetic opertions with it
 
 // the java script won't add any number with string because string can contain any type of data type
 // example : 
 
-// console.log( "Atharva" + 2 );// here you can see why javascript did't perform arithmetic
+console.log( "Atharva" + 2 );// here you can see why javascript did't perform arithmetic
 // //or
-// console.log(2 + "atharva"); // here you can see why in second case from above examples, even though left side was numeric javascript did't perform aritmetic operations
+console.log(2 + "atharva"); // here you can see why in second case from above examples, even though left side was numeric javascript did't perform aritmetic operations
 
-// let gameCounter = 100 
+let gameCounter = 100 
 
-// ++gameCounter; // this is pre increment operator
+++gameCounter; // this is pre increment operator
 
-// gameCounter++; //this is post increament operator
+gameCounter++; //this is post increament operator
 
 // console.log(gameCounter);
 
--- 01_exercises/Day_01.js
+++ 01_exercises/Day_01.js
@@ -1,29 +1,29 @@
-// var accountId = "1234"
-// console.log(accountId);
+var accountId = "1234"
+console.log(accountId);
 
-// let score = "98"
-// console.log(score)
+let score = "98"
+console.log(score)
 
-// const isLoggedIn = true
-// console.log(isLoggedIn);
+const isLoggedIn = true
+console.log(isLoggedIn);
 
-// let phone = 989
-// console.log(typeof phone);
+let phone = 989
+console.log(typeof phone);
 
-// let carname = "virtus"
-// console.log(typeof carname);
+let carname = "virtus"
+console.log(typeof carname);
 
-// let virtusLife = true
-// console.log(virtusLife);
+let virtusLife = true
+console.log(virtusLife);
 
-// let virtus = 10
-// console.log(virtus);
+let virtus = 10
+console.log(virtus);
 
-// let Newvirtus = 100
-// console.log(Newvirtus);
+let Newvirtus = 100
+console.log(Newvirtus);
 
 const hondacity = 0
 console.log(hondacity);
 
-const hondacity = 1
+const hondacity1 = 1
 console.log(hondacity);
