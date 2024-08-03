9735464e2dbb8900c48dd4e96301c4a117ba577c
Atharva-tech-sce <codewithatharva98@gmail.com>
2024-08-03 16:06:20 +0000

    cahanges

--- 01_basics/07_dates_and_time.js
+++ 01_basics/07_dates_and_time.js
@@ -1,21 +1,32 @@
 // Date
 
 let myDate = new Date()
-console.log(myDate);//to print date and time
-console.log(myDate.toString());//To print date and time in more organized way
-console.log(myDate.toDateString());
-console.log(myDate.toISOString());
-console.log(myDate.toJSON);
-console.log(myDate.toLocaleDateString());
-console.log(myDate.toLocaleString());
-console.log(myDate.toLocaleTimeString());
-console.log(myDate.toTimeString());
-console.log(myDate.toUTCString());
-console.log(myDate.getTimezoneOffset());
-console.log();
+
+// console.log(myDate);//to print date and time in format of "YYYY-MM-DDTHH:MM:SS.SSSZ" where T seprate date&time "z" indicate in UTC
+// console.log(myDate.toString());//toString() converts the Date object into a human-readable string format.
+// console.log(myDate.toDateString());//toDateString() extracts only the date part of the Date object, omitting the time.
+// console.log(myDate.toISOString());//toISOString() returns the date in ISO 8601 format, similar to the default string format.
+// console.log(myDate.toJSON());//toJSON() converts the Date object into a JSON-compatible string.Internally, this method calls toISOString(), which is why you see the same output as toISOString().
+// console.log(myDate.toLocaleDateString());//toLocaleDateString() formats the date according to the locale of your environment (likely the default for your system or browser settings).
+// console.log(myDate.toLocaleString());//toLocaleString() provides both the date and time in the format that is localized to your environment.
+// console.log(myDate.toLocaleTimeString());//toLocaleTimeString() extracts only the time portion of the Date object and formats it according to your locale. 12 hour format
+// console.log(myDate.toTimeString());//toTimeString() returns the time portion of the Date object as a string, including the timezone. 24 hour format
+// console.log(myDate.toUTCString());//toUTCString() converts the date to a string formatted in UTC. he format includes the day of the week, day of the month, month, year, time, and GMT
+// console.log(myDate.getTimezoneOffset());//Returns the difference in minutes between your local timezone and UTC.
+
+// console.log(typeof myDate);
+
+// to declare specific or new date
+
+let myCreatedDate = new Date(2024, 8, 3);//In javascript months starts from 0, at the end 3 denoted to day that's why it cannot be start with "0" otherwise it'll be consfusing with dates
+console.log(myCreatedDate.toDateString());
+
+let myCreatedDate1 = new Date(2024, 8, 3, 9, 8, 30)
+console.log(myCreatedDate1.toLocaleString());s
 
 
 
-// only date and time
+
+
 
 
